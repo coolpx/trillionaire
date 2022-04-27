1d3d1382702ae8bfd3f2504de0a2bb56a1cb9978
coolpx <coolp_xels@proton.me>
2022-04-27 10:20:56 -0500

    Remove useless files; beginnings of saving account

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -36,6 +36,7 @@ local list = Instance.new("UIListLayout", purchaseList)
 list.Padding = UDim.new(0, 8)
 
 local cashDisplay = Instance.new("TextLabel", mainFrame)
+cashDisplay.Name = "CashLabel"
 cashDisplay.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 cashDisplay.Size = UDim2.new(.4, -8, .2, -16)
 cashDisplay.Position = UDim2.new(.5, 0, 0, 8)
@@ -43,13 +44,71 @@ cashDisplay.AnchorPoint = Vector2.new(.5, 0)
 cashDisplay.BorderColor3 = Color3.fromRGB(160, 160, 160)
 cashDisplay.TextScaled = true
 
+local locationDisplay = Instance.new("TextLabel", mainFrame)
+locationDisplay.Text = [[0 Locations]]
+locationDisplay.Name = "LocationLabel"
+locationDisplay.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+locationDisplay.Size = UDim2.new(.4, -8, .2, -8)
+locationDisplay.Position = UDim2.new(.5, 0, .2, 0)
+locationDisplay.AnchorPoint = Vector2.new(.5, 0)
+locationDisplay.BorderColor3 = Color3.fromRGB(160, 160, 160)
+locationDisplay.TextScaled = true
+locationDisplay.Visible = false
+
+local franchiseDisplay = Instance.new("TextLabel", mainFrame)
+franchiseDisplay.Text = [[0 Franchises]]
+franchiseDisplay.Name = "FranchiseLabel"
+franchiseDisplay.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+franchiseDisplay.Size = UDim2.new(.4, -8, .2, -8)
+franchiseDisplay.Position = UDim2.new(.5, 0, .4, 0)
+franchiseDisplay.AnchorPoint = Vector2.new(.5, 0)
+franchiseDisplay.BorderColor3 = Color3.fromRGB(160, 160, 160)
+franchiseDisplay.TextScaled = true
+franchiseDisplay.Visible = false
+
+local bankDisplay = Instance.new("TextLabel", mainFrame)
+bankDisplay.Name = "FranchiseLabel"
+bankDisplay.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+bankDisplay.Size = UDim2.new(.4, -8, .2, -8)
+bankDisplay.Position = UDim2.new(.5, 0, .6, 0)
+bankDisplay.AnchorPoint = Vector2.new(.5, 0)
+bankDisplay.BorderColor3 = Color3.fromRGB(160, 160, 160)
+bankDisplay.TextScaled = true
+bankDisplay.Visible = false
+
+local bankAdd = Instance.new("TextButton", mainFrame)
+bankAdd.Name = "BankAdd"
+bankAdd.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+bankAdd.Size = UDim2.new(.2, -8, .2, -8)
+bankAdd.Position = UDim2.new(.4, 0, .8, 0)
+bankAdd.AnchorPoint = Vector2.new(.5, 0)
+bankAdd.BorderColor3 = Color3.fromRGB(160, 160, 160)
+bankAdd.TextScaled = true
+bankAdd.Text = "+"
+bankAdd.Visible = false
+
+local bankSub = Instance.new("TextButton", mainFrame)
+bankSub.Name = "BankSub"
+bankSub.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+bankSub.Size = UDim2.new(.2, -8, .2, -8)
+bankSub.Position = UDim2.new(.6, 0, .8, 0)
+bankSub.AnchorPoint = Vector2.new(.5, 0)
+bankSub.BorderColor3 = Color3.fromRGB(160, 160, 160)
+bankSub.TextScaled = true
+bankSub.Text = "-"
+bankSub.Visible = false
+
 local day = 0
 local interest = 0.08
 
 local locations = 0
+local franchises = 0
 local cash = 3000
 local incomePerLocation = 150
-local bank = 0
+local banked = 0
+
+local franchisesEnabled = false
+local bankingEnabled = false
 
 local function activationListener(item: GuiButton, func)
     item.MouseButton1Click:Connect(func)
@@ -71,15 +130,41 @@ purchases = {
         Description = "Your very first store. Generates $150 per day.",
         Action = function()
             locations += 1
+            locationDisplay.Text = [[1 Location]]
+            locationDisplay.Visible = true
+        end
+    },
+    {
+        DoNotDelete = true,
+        Dependency = "First Location",
+        Name = "Another Location",
+        Price = 1500,
+        Description = "Get another store to increase profits.",
+        Action = function()
+            locations += 1
+            locationDisplay.Text = tostring(locations) .. " Locations"
         end
     },
     {
         Dependency = "First Location",
-        Name = "Second Location",
-        Price = 1500,
-        Description = "Get another store to increase profits",
+        Name = "Franchise",
+        Price = 15000,
+        Description = "Franchise your store so other people can create more locations. You will only get half of regular revenue from franchised stores, however.",
         Action = function()
-            locations += 1
+            franchisesEnabled = true
+            franchiseDisplay.Visible = true
+        end
+    },
+    {
+        Dependency = "First Location",
+        Name = "Savings Account",
+        Price = 1500,
+        Description = "Get a savings account to earn interest on your cash over time.",
+        Action = function()
+            bankingEnabled = true
+            bankDisplay.Visible = true
+            bankAdd.Visible = true
+            bankSub.Visible = true
         end
     }
 }
@@ -99,7 +184,7 @@ local price = Instance.new("TextLabel", baseButton)
 price.BackgroundTransparency = 1
 price.Name = "Price"
 price.Size = UDim2.new(1, -8, .3, -8)
-price.Position = UDim2.new(0, 4, .3, 4)
+price.Position = UDim2.new(0, 4, .25, 4)
 price.TextScaled = true
 
 local description = Instance.new("TextLabel", baseButton)
@@ -108,8 +193,8 @@ description.TextYAlignment = Enum.TextYAlignment.Top
 description.TextWrapped = true
 description.BackgroundTransparency = 1
 description.Name = "Description"
-description.Size = UDim2.new(1, -8, .4, -8)
-description.Position = UDim2.new(0, 4, .6, 4)
+description.Size = UDim2.new(1, -8, .45, -8)
+description.Position = UDim2.new(0, 4, .55, 4)
 
 local uiStroke = Instance.new("UIStroke", baseButton)
 uiStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
@@ -150,7 +235,9 @@ for i, purchase in pairs(purchases) do
         activationListener(button, function()
             if cash >= purchase.Price then
                 button:Destroy()
-                table.remove(purchases, i)
+                if not purchase["DoNotDelete"] then
+                    table.remove(purchases, i)
+                end
                 cash = cash - purchase.Price
                 purchase.Action()
                 cashDisplay.Text = "$" .. tostring(cash)
@@ -162,7 +249,16 @@ end
 cashDisplay.Text = "$" .. tostring(cash)
 while true do
     task.wait(1.5)
+
     day += 1
     cash += locations * incomePerLocation
     cashDisplay.Text = "$" .. tostring(cash)
+
+    if franchisesEnabled then
+        if math.random() > .8 then
+            franchises += 1
+            franchiseDisplay.Text = tostring(franchises) .. " Franchises"
+        end
+        cash += franchises * (incomePerLocation / 2)
+    end
 end
